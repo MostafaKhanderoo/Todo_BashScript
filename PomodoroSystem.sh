1a1eec24579a84b877129a1e192e0221e2148464
mostafa <mostafakhanderoo7@gmail.com>
2024-12-10 17:20:40 +0330

    create PomodoroSystem to Onlooker Time work

---
+++ PomodoroSystem.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+read -p "entre a number of test work " sessions
+
+
+for ((i=1; i<=sessions; i++)); do
+  echo "start number  $i..."
+  sleep 10
+  echo "finished number $i."
+
+  if ((i % 4 == 0)); then
+    echo "rest 5 second"
+    sleep 5
+  else
+    echo "rest 3 second"
+    sleep 3
+  fi
+done
+
+echo "finish goodLuck!"
