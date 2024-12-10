548217cb94203b2bebf5adc7dfd0715f23f85319
mostafa <mostafakhanderoo7@gmail.com>
2024-12-10 16:26:47 +0330

    add functional method show menu for user

--- todo.sh
+++ todo.sh
@@ -5,3 +5,15 @@ Todo_FILE = "todo.txt"
 Done_FILE = "done.txt"
 
 echo $Todo_FILE
+
+ show_menu() {
+  echo "-------------------------"
+  echo "ToDo Management"
+  echo "1) add new task"
+  echo "2) delete task"
+  echo "3) view all task"
+  echo "4) serch in list"
+  echo "5) Exist"
+  echo "-------------------------"
+  echo "tsst "
+}
